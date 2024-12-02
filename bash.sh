be9113b1b8781063558c3cab10b41e5bf126aead
root <root@Linux-RemoteWebServer.i1nmttrzutauzmwpbivigvvelh.bx.internal.cloudapp.net>
2024-12-02 01:50:46 +0000

    Hello Commit1

--- bash.sh
+++ bash.sh
@@ -1,4 +1,4 @@
 yum install httpd -y
-echo "<h1> Hai i am Linux WebServer Scripting Running in Azure.This is a Version 2 </h1>"> /var/www/html/index.html
+echo "<h1> Hai i am Linux WebServer Scripting Running in Azure.This is a Version 2. I have Modified Via Azure VM </h1>"> /var/www/html/index.html
 service httpd start
 chkconfig httpd on
